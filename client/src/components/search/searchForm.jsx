a7ff2b28ae881f4c95cbee7afa7c6ba4b5e15e4f
acantu9 <adrianaacantu@gmail.com>
2024-01-23 21:20:19 -0600

    added css styling to search form

--- client/src/components/search/searchForm.jsx
+++ client/src/components/search/searchForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '../../styles/searchForm.css';
 
 const SearchForm = () => {
   const [petType, setPetType] = useState('');
@@ -40,10 +41,10 @@ const SearchForm = () => {
   }
 
   return (
-    <form onSubmit={handleFormSubmit}>
+    <form onSubmit={handleFormSubmit} className="search-form">
       <label>
         Pet Type:
-        <select value={petType} onChange={(e) => setPetType(e.target.value)}>
+        <select className="form-select mb-3" value={petType} onChange={(e) => setPetType(e.target.value)}>
           <option value="">Select a pet type</option>
           <option value="dog">Dog</option>
           <option value="cat">Cat</option>
@@ -54,7 +55,7 @@ const SearchForm = () => {
 
       <label>
         Pet Size:
-        <select value={petSize} onChange={(e) => setPetSize(e.target.value)}>
+        <select className="form-select mb-3" value={petSize} onChange={(e) => setPetSize(e.target.value)}>
           <option value="">Select a pet size</option>
           <option value="small">Small</option>
           <option value="medium">Medium</option>
@@ -64,7 +65,7 @@ const SearchForm = () => {
 
       <label>
         Pet Breed:
-        <select value={petBreed} onChange={(e) => setPetBreed(e.target.value)}>
+        <select className="form-select mb-3" value={petBreed} onChange={(e) => setPetBreed(e.target.value)}>
           <option value="">Select a pet breed</option>
           {availableBreeds.map((breed) => (
             <option key={breed} value={breed}>
@@ -76,7 +77,7 @@ const SearchForm = () => {
 
       <label>
         Pet Age:
-        <select value={petAge} onChange={(e) => setPetAge(e.target.value)}>
+        <select className="form-select mb-3" value={petAge} onChange={(e) => setPetAge(e.target.value)}>
           <option value="">Select a pet age</option>
           <option value="young">Young</option>
           <option value="adult">Adult</option>
@@ -86,7 +87,7 @@ const SearchForm = () => {
 
       <label>
         Pet Gender:
-        <select value={petGender} onChange={(e) => setPetGender(e.target.value)}>
+        <select className="form-select mb-3" value={petGender} onChange={(e) => setPetGender(e.target.value)}>
           <option value="">Select a pet gender</option>
           <option value="male">Male</option>
           <option value="female">Female</option>
@@ -94,15 +95,15 @@ const SearchForm = () => {
       </label>
 
       <label>
-        Is Pet Spay/Neutered?:
-        <select value={petSpayNeuter} onChange={(e) => setPetSpayNeuter(e.target.value)}>
+        Spay/Neutered Pet?:
+        <select className="form-select mb-4" value={petSpayNeuter} onChange={(e) => setPetSpayNeuter(e.target.value)}>
           <option value="">Select an option</option>
           <option value="yes">Yes</option>
           <option value="no">No</option>
         </select>
       </label>
 
-      <button type="submit">Search</button>
+      <button class="btn btn-primary btn-lg" type="submit">Search</button>
     </form>
   );
 };
---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,12 @@
+import SearchForm from "../components/search/searchForm";
+
+const Search = () => {
+    return(
+        <div>
+            <h1 className="h1 mb-10"> Let's Find Your Purrfect Pet! </h1>
+            <SearchForm />
+        </div>
+    )
+}
+
+export default Search;
