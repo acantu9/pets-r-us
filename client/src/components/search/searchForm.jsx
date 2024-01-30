f11fd8cfca7544b30154e093fc2698ef94b77f60
acantu9 <adrianaacantu@gmail.com>
2024-01-29 22:59:31 -0600

    debug api

--- client/src/components/search/searchForm.jsx
+++ client/src/components/search/searchForm.jsx
@@ -6,16 +6,16 @@ const SearchForm = ({ onSearch }) => {
   const CLIENT = "OrQk1R8l99Br87khJTK3eYpXOyXXU7oe8t7gP4mQLrc688TQUh";
   const SECRET = "E31rZA9nHMB2qgzai5EWSIyU3bsmTZGATh7T0SCs";
   // function to get api token - valid for 1 hour
-  fetch('https://api.petfinder.com/v2/oauth2/token', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  },
-  body: `grant_type=client_credentials&client_id=${CLIENT}&client_secret=${SECRET}`
-  }).then(data => { return data.json() })
-    .then( token => { console.log(token) });
-    // const APITOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJPc…0gpOitXqoSNIbv0HbaVED0QPwXcy2pJAuNlKClivndfwmtqMw";
-    const APITOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJPclFrMVI4bDk5QnI4N2toSlRLM2VZcFhPeVhYVTdvZTh0N2dQNG1RTHJjNjg4VFFVaCIsImp0aSI6IjhiZDJiMGZlNjQyNTAwYjBmOGU4YTY1MWJkMjQ5MmZiMWEwOTE1OWI3Mjc4NTFkMGU5YzZkODgxMjFlNDAzOTMzYjRkNGU0YWVjYmFkM2FjIiwiaWF0IjoxNzA2MjQ0NTgyLCJuYmYiOjE3MDYyNDQ1ODIsImV4cCI6MTcwNjI0ODE4Miwic3ViIjoiIiwic2NvcGVzIjpbXX0.FmPjg961O298Leili5zFHf7zwnkidyb66JD_IOab3jMzb0vyfhe3gDmjAxjhzbPNbEutFPT6fQSQAwORUXaBn-Ui4o6QD6VLAkqjohszVy4BxQshdL1IT2qztKLnsRD3uFSH4ILtARlR2pzmQEFtXqZfOlcIAkiU2-1Kyo8ZxEzzXV1Yl1I-oZBy_ADE552rtoEkqNnaJzGnC4O5Fj5lZCtOCpUOLMrSdpwnxsUwX1CSlvMA46Td1mFzBgYcTrYSKy95e-jBedwcMgdACN3hTnLc0sH7w_KIlWEQWg4qSdUDjp6hGtATnOBi0hrCE9m_O68KXqftt9kv9k3s6FE6Nw";
+  // fetch('https://api.petfinder.com/v2/oauth2/token', {
+  // method: 'POST',
+  // headers: {
+  //   'Content-Type': 'application/x-www-form-urlencoded'
+  // },
+  // body: `grant_type=client_credentials&client_id=${CLIENT}&client_secret=${SECRET}`
+  // }).then(data => { return data.json() })
+  //   .then( token => { console.log(token) });
+  // const APITOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJPc…0gpOitXqoSNIbv0HbaVED0QPwXcy2pJAuNlKClivndfwmtqMw";
+  const APITOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJPclFrMVI4bDk5QnI4N2toSlRLM2VZcFhPeVhYVTdvZTh0N2dQNG1RTHJjNjg4VFFVaCIsImp0aSI6IjAxNmI5MGQyOTE0MWI4YWQ1M2U5YjIzNThmZmQ1ODlhOTM3MThlYTZhNTBlYTYxNWNmNjVlODBiZDFkNmNjZWQyZWRlNGM2NzRhMzJhZGU1IiwiaWF0IjoxNzA2NTg3MTE2LCJuYmYiOjE3MDY1ODcxMTYsImV4cCI6MTcwNjU5MDcxNiwic3ViIjoiIiwic2NvcGVzIjpbXX0.dIYl_yrNw2RXcGZ2sEOqkksKRgDshTrzRSOxar33k3_KvaRSgoxlMmnUtkfy_AzzSICOwv-bxkGzi6E2H8shEK8deYvDPgGu6QQYsW_sNHZZb2bZmsypnWi3AY0IrACYfMsmH23hT4odjP1BizazC5aURyC6fxONMlIxcLOVf9UGkIs4FYLmqyOyoxo9jBmQDI8rAGAoXD9_sd3Xfua8imRQm1JpvczV6FBYgqCOSED8Kf4wuay1rJHUwngX4LroJW_R2ZJBGNN_JBuSiWOqLySQOOfSWJUrDcTXAE1E_F5dQHp3bFQ-SN9m6FbRQR4yJFiOyYTeBVX3FAg8X2_l2g";
   const [petType, setPetType] = useState('');
   const [petSize, setPetSize] = useState('');
   const [petBreed, setPetBreed] = useState('');
@@ -28,7 +28,9 @@ const SearchForm = ({ onSearch }) => {
     console.log("2: Call submit function");
     onSearch();
     // Construct API URL with selected options
-     const apiUrl = `https://api.petfinder.com/pets?petType=${petType}&petSize=${petSize}&petBreed=${petBreed}&petAge=${petAge}&petGender=${petGender}&petSpayNeuter=${petSpayNeuter}`;
+     const apiUrl = `/get-animals?` + new URLSearchParams({
+      type: petType,
+   });
 
      console.log(apiUrl);
      console.log(`Bearer ${APITOKEN}`); 
@@ -140,3 +142,32 @@ const SearchForm = ({ onSearch }) => {
 };
 
 export default SearchForm;
+
+
+// import React, { useState } from 'react';
+
+// const SearchForm = ({ onSearch }) => {
+//   const [searchQuery, setSearchQuery] = useState('');
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     if (searchQuery.trim() !== '') {
+//       // Perform any additional validation or formatting if needed
+//       onSearch(searchQuery);
+//     }
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input
+//         type="text"
+//         value={searchQuery}
+//         onChange={(e) => setSearchQuery(e.target.value)}
+//         placeholder="Enter your search query"
+//       />
+//       <button type="submit">Search</button>
+//     </form>
+//   );
+// };
+
+// export default SearchForm;
