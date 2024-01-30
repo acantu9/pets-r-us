f5bdb01b77629d28a6cdcbf4a9b8e989167eaa20
Cfrog5 <cjfraga05@gmail.com>
2024-01-29 22:18:22 -0600

    color adjustments

--- client/src/pages/User-Profile.jsx
+++ client/src/pages/User-Profile.jsx
@@ -21,7 +21,7 @@ function user_profile() {
 return(
 <main>
 {/* User Profile Card */}
-<div class="card mb-3" style= {{maxWidth:"540px", backgroundColor:"#050038", borderstyle: "solid"}}> 
+<div class="card mb-3" style= {{maxWidth:"540px", backgroundColor:"#050038", borderstyle:"solid", borderWidth:"3px", borderColor:"Black"}}> 
   <div class="row g-0">
     <div class="col-md-4">
     <IoPersonCircleOutline size="200px"/>
@@ -29,7 +29,8 @@ return(
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">User Placeholder</h5>
+        <h4 class="card-type" >Profile</h4>
+        <h5 class="card-user">User Placeholder</h5>
         <p class="card-text"> User email placeholder </p>
       </div>
     </div>
